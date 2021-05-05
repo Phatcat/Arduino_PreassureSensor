869fa94d242ac0a85055317217ccc731faf73504
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2021-05-05 13:07:23 +0200

    Update PreassureSensor
    Fourth major revision to the code.
    Mainly focuses on additional display options, display range, auto scaling, out of display range error reporting and more.

--- PreassureSensor/PreassureSensor.ino
+++ PreassureSensor/PreassureSensor.ino
@@ -4,7 +4,7 @@
 #define AVERAGE 32
 
 // Define the number of pixels on the screen to use for the graph (x-axis)
-#define PIXELCOUNT 118
+#define PIXELCOUNT 114
 
 // Set and use the correct mode for the display (ST7920, 128x64, SPI)
 U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
@@ -12,24 +12,43 @@ U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
 // Create array for graphical display of sensor value in graph form and nullify all entries
 float graphArray[PIXELCOUNT] = {0.0};
 
-unsigned int zeroPixelY = 52;
-int scale = 10;
+int zeroPixelY = 52;
+uint8_t scale = 10;
 
 unsigned int currentValue = 0;
-unsigned int i = 1;
-float peakKpa = 0.0;
+uint8_t i = 1;
+
 float zeroVoltage = 0.0;
+float peakKpa = 0.0;
+bool overLoad = false;
 
 bool firstFill = true;
-unsigned int fillCounter = 0;
+uint8_t fillCounter = 0;
 
 bool holdGraph = false;
+bool autoScale = true;
+
+int minDisplayValue = 0;
+bool outOfDisplayRange = false;
+
+bool printGraph = false;
 
 void setup()
 {
+  // Calibrate
   pinMode(2, INPUT_PULLUP);
+  // Reset
   pinMode(3, INPUT_PULLUP);
+  // Hold/Go
   pinMode(4, INPUT_PULLUP);
+  // Scale
+  pinMode(5, INPUT_PULLUP);
+  // Display range up
+  pinMode(6, INPUT_PULLUP);
+  // Display range down
+  pinMode(7, INPUT_PULLUP);
+  // Print graph
+  pinMode(12, INPUT_PULLUP);
 
   Serial.begin(9600); 
   u8g2.begin();
@@ -45,16 +64,64 @@ void loop()
   u8g2.firstPage();
   do
   {
-    drawBars();
+    drawBase();
 
+    // Hold / Go
     if(digitalRead(4) == LOW)
     {
       // Hault the program to make sure the state only switches once per button press
       do { ; } while ( digitalRead(4) == LOW );
+
       // Switch hold state on button release
       holdGraph = !holdGraph;
     }
 
+    // Print Graph
+    if(digitalRead(12) == LOW)
+    {
+      // Hault the program to make sure the state only switches once per button press
+      do { ; } while ( digitalRead(12) == LOW );
+
+      // Switch print state on button release
+      printGraph = !printGraph;
+    }
+
+    // Switch scale
+    if(digitalRead(5) == LOW)
+    {
+      if(autoScale)
+        autoScale = false;
+      else if(scale > 1)
+        switchScale();
+      else
+      {
+        scale = 10;
+        autoScale = true;
+      }
+
+      // Hault the program to make sure the state only switches once per button press
+      do { ; } while ( digitalRead(5) == LOW );
+    }
+
+    // Display range
+    if(digitalRead(6) == LOW)
+    {
+      if((minDisplayValue + (50 / scale)) < 50)
+      minDisplayValue += 5;
+
+      // Hault the program to make sure the state only switches once per button press
+      do { ; } while ( digitalRead(6) == LOW );
+    }
+    else if(digitalRead(7) == LOW)
+    {
+      if(minDisplayValue > -50)
+        minDisplayValue -= 5;
+
+      // Hault the program to make sure the state only switches once per button press
+      do { ; } while ( digitalRead(7) == LOW );
+    }
+
+    // Calibration / reset
     if(digitalRead(2) == LOW)
       drawConfig(true);
     else if(digitalRead(3) == LOW)
@@ -68,96 +135,131 @@ void loop()
   } while( u8g2.nextPage() );
 }
 
-void drawBars()
+void drawBase()
 {
 /*********** Draw border frames *************/
-  u8g2.drawLine(9, 0, 9, 55);
-  u8g2.drawLine(9, 55, 127, 55);
+  u8g2.drawLine(13, 0, 13, 56);
+  u8g2.drawLine(13, 56, 127, 56);
 /******* Draw reference pixel points ********/
-  u8g2.drawPixel(8, 2);
-  u8g2.drawPixel(8, 12);
-  u8g2.drawPixel(8, 22);
-  u8g2.drawPixel(8, 32);
-  u8g2.drawPixel(8, 42);
-  u8g2.drawPixel(8, 52);
+  u8g2.drawPixel(12, 2);
+  u8g2.drawPixel(12, 12);
+  u8g2.drawPixel(12, 22);
+  u8g2.drawPixel(12, 32);
+  u8g2.drawPixel(12, 42);
+  u8g2.drawPixel(12, 52);
 /********************************************/
 
-  // Set font for graph reference points
   u8g2.setFont(u8g2_font_blipfest_07_tr);
 
 /************ Reference values **************/
-  int k = 4;
-  if(scale < 5)
-    k = 0;
-  else if(scale < 10)
-    k = 1;
-  u8g2.setCursor(k, 5);
-  u8g2.print(50 / scale);
+  for(uint8_t j = 0; j < 6; j++)
+  {
+    int refValue = minDisplayValue;
+    if(j > 0)
+      refValue += (j * 10) / scale;
 
-  k = 4;
-  if(scale < 5)
-    k = 0;
-  u8g2.setCursor(k, 15);
-  u8g2.print(40 / scale);
+    uint8_t k = 0;
 
-  k = 4;
-  if(scale < 2)
-    k = 0;
-  else if(scale < 5)
-    k = 1;
-  u8g2.setCursor(k, 25);
-  u8g2.print(30 / scale);
+    if(refValue >= 20)
+      k = 4;
+    else if(refValue >= 10 || refValue == -1)
+      k = 5;
+    else if(refValue >= 2 || refValue == 0)
+      k = 8;
+    else if(refValue == 1)
+      k = 9;
+    else if(refValue <= -20)
+      k = 0;
+    else if(refValue <= -10)
+      k = 1;
+    else if(refValue <= 2)
+      k = 4;
 
-  k = 4;
-  if(scale < 2)
-    k = 0;
-  else if(scale < 5)
-    k = 1;
-  u8g2.setCursor(k, 35);
-  u8g2.print(20 / scale);
-
-  k = 4;
-  if(scale < 2)
-    k = 1;
-  u8g2.setCursor(k, 45);
-  u8g2.print(10 / scale);
-
-  u8g2.setCursor(4, 55);
-  u8g2.print(0);
+    u8g2.setCursor(k, 55 - (10 * j));
+    u8g2.print(refValue);
+  }
 /********************************************/
 
-  u8g2.setCursor(0, 64);
-  if(holdGraph)
-    u8g2.print("HO");
-  else
-    u8g2.print("GO");
+  if(holdGraph) // ⏸
+  {
+    u8g2.drawLine(2, 59, 2, 62);
+    u8g2.drawLine(4, 59, 4, 62);
+  }
+  else // ⏵
+  {
+    u8g2.drawLine(2, 58, 2, 62);
+    u8g2.drawLine(3, 59, 3, 61);
+    u8g2.drawPixel(4, 60);
+  }
+
+  if(printGraph)
+  {
+    u8g2.setCursor(8, 63);
+    u8g2.print("P");
+  }
+
+  u8g2.setCursor(15, 63);
+  u8g2.print(scale);
+  u8g2.print(":1");
+
+  if(autoScale)
+  {
+    u8g2.setCursor(32, 63);
+    u8g2.print("AUTO");
+  }
+
+  if(outOfDisplayRange)
+  {
+    u8g2.setCursor(52, 63);
+    u8g2.print("OODR");
+  }
 
   // Set font fitting to the bottom frame
-  u8g2.setFont(u8g2_font_courB08_tr);
+  // u8g2.setFont(u8g2_font_courB08_tr);
 
   // Fill in text and peak value
-  u8g2.setCursor(12, 64);
-  u8g2.print("Peak kPa: ");
-  u8g2.setCursor(67, 64);
-  u8g2.print(peakKpa, 7);
+  u8g2.setCursor(75, 63);
+  u8g2.print("PEAK: ");
+    
+  if(overLoad)
+    u8g2.print("OVERLOAD");
+  else
+    u8g2.print(peakKpa, 6);
 }
 
 void drawGraph()
 {
-  // Draw the graph from the array with a 0-point at zeroPixelY and an absolute leftmost x-point of 10 (horizontal border line + 1)
-  for(int j = 0; j < (firstFill ? fillCounter : PIXELCOUNT); j++)
+  outOfDisplayRange = false;
+
+  int maxDisplayValue = minDisplayValue + (50 / scale);
+  
+  if(maxDisplayValue <= 0)
+    zeroPixelY = 2 + maxDisplayValue * scale;
+  else
+    zeroPixelY = 52 + minDisplayValue * scale;
+  
+  // Draw the graph from the array with a 0-point at zeroPixelY and an absolute leftmost x-point of 14 (horizontal border line + 1)
+  for(uint8_t j = 0; j < (firstFill ? fillCounter : PIXELCOUNT); j++)
   {
-    // Pixel graph
-    // u8g2.drawPixel(10 + j, zeroPixelY - round(graphArray[j]));
-
-    // Bar graph
-    // u8g2.drawLine(10 + j, zeroPixelY, 10 + j, zeroPixelY - round(graphArray[j]));
-
+    if(graphArray[j] > maxDisplayValue)
+    {
+      u8g2.drawPixel(14 + j, 0);
+      outOfDisplayRange = true;
+    }
+    else if(graphArray[j] < (minDisplayValue - (2 / scale)))
+    {
+      u8g2.drawPixel(14 + j, 55);
+      outOfDisplayRange = true;
+    }
+    else
+    {
+      uint8_t displayValue = zeroPixelY - round(graphArray[j] * scale);
 /***** Continuous line graph ****************/
-    u8g2.drawPixel(10 + j, zeroPixelY - round(graphArray[j]));
-    if(j > 0)
-      u8g2.drawLine(9 + j, zeroPixelY - round(graphArray[j - 1]), 10 + j, zeroPixelY - round(graphArray[j]));
+      u8g2.drawPixel(14 + j, displayValue);
+        if(j > 0)
+      u8g2.drawLine(13 + j, zeroPixelY - round(graphArray[j - 1] * scale), 14 + j, displayValue);
 /********************************************/
+    }
   }
 }
 
@@ -174,6 +276,7 @@ void drawConfig(bool calibrating)
 
   fillCounter = 0;
   firstFill = true;
+  overLoad = false;
 
   u8g2.setFont(u8g2_font_courB08_tr);
   u8g2.setCursor(30, 36);
@@ -200,32 +303,36 @@ void calculate()
   
     // Calculate voltage into kPa
     float currentKpa = (((currentVoltage / 5.0) - 0.04) / 0.018) - (((zeroVoltage / 5.0) - 0.04) / 0.018);
-  
+
+    if(printGraph)
+      Serial.println(currentKpa);
+
+    if(autoScale && (currentKpa > (50 / scale)))
+      switchScale();
+
     if(currentKpa > peakKpa)
     {
-      peakKpa = currentKpa;
-      if(peakKpa > (50.0 / scale))
-      {
-        int k = scale;
-  
-        if(peakKpa >= 25.0)
-          scale = 1;
-        else if(peakKpa >= 10.0)
-          scale = 2;
-        else if(peakKpa >= 5.0)
-          scale = 5;
-  
-        for(int j = 0; j < PIXELCOUNT; j++)
-          graphArray[j] = graphArray[j] / k;
-      }
+      if(currentKpa <= 50.0)
+        peakKpa = currentKpa;
+      else
+        overLoad = true;
     }
+    else if((currentKpa < minDisplayValue) && autoScale)
+    {
+      if(minDisplayValue <= 0)
+        switchScale();
+      else
+        minDisplayValue -= 5;
+    }
+    else if((currentKpa > (minDisplayValue + (50 / scale))) && autoScale)
+      minDisplayValue += 5;
 
     // Go from right to left for the first fill or after a reset/calibration
     if(firstFill)
     {
       if(fillCounter < PIXELCOUNT)
       {  
-        graphArray[fillCounter] = currentKpa * scale;
+        graphArray[fillCounter] = currentKpa;
         fillCounter++;
       }
       else
@@ -236,10 +343,10 @@ void calculate()
     }
     else
     {
-      for(int j = 0; j < (PIXELCOUNT - 1); j++)
+      for(uint8_t j = 0; j < (PIXELCOUNT - 1); j++)
         graphArray[j] = graphArray[j + 1];
   
-      graphArray[PIXELCOUNT - 1] = currentKpa * scale;
+      graphArray[PIXELCOUNT - 1] = currentKpa;
     }
 
     currentValue = 0;
@@ -247,4 +354,14 @@ void calculate()
   }
   else
     i++;
+}
+
+void switchScale()
+{
+  if(scale == 10)
+    scale = 5;
+  else if(scale == 5)
+    scale = 2;
+  else if(scale == 2)
+    scale = 1;
 }
